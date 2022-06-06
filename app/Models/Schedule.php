4221fc13d4c15e39025e5de42e7105e4f28bd7ab
Basim Ghouri <97091625+ghourigeeks@users.noreply.github.com>
2022-06-07 00:16:32 +0500

    Update Schedule.php

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -35,13 +35,15 @@ class Schedule extends Model
         'cancel_reason',
         
         'status_id',
-
+        
         'show_contact',
-
-        'ride_start',
-
-        'ride_end',
-
+        
+        'start_time',
+        'end_time',
+        
+        'start_date',
+        'end_date',
+        
         'active'
     ];
 
